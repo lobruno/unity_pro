0da63fa7e38f0e0264cf0916f56002e946e10e22
lobruno <daniarbeknurbol@gmail.com>
2021-09-29 21:51:04 +0300

    new
    new

---
+++ new project/Assets/move.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float a = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (a > 0.5)
+        {
+            a -= Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, 10) * Time.deltaTime);
+        }
+        else if (a < 0) { a = 1.0f; }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, -20) * Time.deltaTime);
+        }
+        
+    }
+}
